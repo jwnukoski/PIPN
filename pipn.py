cf37f7826ef895f34e6a6a3d01c7110e520c4bd2
jwnukoski <jwnukoski@protonmail.com>
2021-01-03 19:07:51 -0500

    init refactor

--- pipn.py
+++ pipn.py
@@ -1,12 +1,12 @@
-import smtplib, ssl, time
+import smtplib, time
 from json import load
+
 try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
 
 def getPublicIp():
-    # Uses ipify
     try:
         return load(urlopen('https://api.ipify.org/?format=json'))['ip']
     except Exception as e:
@@ -16,32 +16,25 @@ def getPublicIp():
 def sendEmail(_smtpServer, _smtpPort, _senderEmail, _password, _receiverEmail, _msg):
     print("Trying to send email notification about IP being: " + _msg)
     try:
-        context = ssl.create_default_context()
-        with smtplib.SMTP(_smtpServer, _smtpPort) as server:
-            server.ehlo()  # Can be omitted
-            server.starttls(context=context)
-            server.ehlo()  # Can be omitted
-            server.login(_senderEmail, _password)
-            server.sendmail(_senderEmail, _receiverEmail, _msg)
+        # ???
+
         print("Email sent for updated IP: " + _msg)
     except Exception as e:
         print("Error sending email: " + str(e))
 
-# Change your settings here if you dont want to use google SMTP, or
-# dont want to input your settings each time. DONT save your password in here.
-smtpPort = int(input("Enter the SMTP port (Google: 587): "))  # For starttls
+smtpPort = int(raw_input("Enter the SMTP port (Google: 587): "))
 smtpServer = raw_input("Enter the SMTP server (Google: smtp.gmail.com): ")
 senderEmail = raw_input("Sender email address: ")
-if (senderEmail.__contains__("@gmail.com")):
-    # Tip for gmail users.
-    print("\nLooks like you are using Gmail. Make sure Less secure app access is on.\nRead more here: https://myaccount.google.com/lesssecureapps\n")
-receiverEmail = raw_input("Receiver email address: ")
 password = raw_input("Type your password and press enter: ")
 
-# Time in seconds you want to grab your public IP
-checkInterval = int(input("Enter the public IP query interval in minutes: ")) * 60
+# Tip for gmail users.
+if (senderEmail.__contains__("@gmail.com")):
+    print("\nLooks like you are using Gmail. Make sure Less secure app access is on.\nRead more here: https://myaccount.google.com/lesssecureapps\n")
 
-# Doesnt need changed.
+receiverEmail = raw_input("Receiver email address: ")
+
+checkInterval = int(raw_input("Enter the public IP query interval in minutes: ")) * 60
+
 cachedIp = ''
 
 while(True):
